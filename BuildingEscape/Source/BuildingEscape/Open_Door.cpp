5da84d4d1e27a2adb5e65203b28f335f3802cd2c
PakHuntMaster <azeemiqbalbhatti@gmail.com>
2019-04-26 03:23:07 +0500

    Update Video #16

--- BuildingEscape/Source/BuildingEscape/Open_Door.cpp
+++ BuildingEscape/Source/BuildingEscape/Open_Door.cpp
@@ -1,11 +1,11 @@
 // Copyright Azeem Iqbal 2019
 
 
-#include "Position_Report.h"
+#include "Open_Door.h"
 #include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
-UPosition_Report::UPosition_Report()
+UOpen_Door::UOpen_Door()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,21 +16,26 @@ UPosition_Report::UPosition_Report()
 
 
 // Called when the game starts
-void UPosition_Report::BeginPlay()
+void UOpen_Door::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("The object %s is at %s"), *ObjectName, *ObjectPos);
-	
+	// Find the owning Actor 
+	AActor* Owner = GetOwner();
+
+	// Create a rotator
+	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+
+	// Set the door rotation
+	Owner->SetActorRotation(NewRotation);
 }
 
 
 // Called every frame
-void UPosition_Report::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpen_Door::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
 }
+
---
+++ BuildingEscape/Source/BuildingEscape/Open_Door.h
@@ -0,0 +1,28 @@
+// Copyright Azeem Iqbal 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Open_Door.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpen_Door : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpen_Door();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
